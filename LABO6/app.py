2cedfde0d883d1c4ce6d2fde7afc17aeee915e50
Osorio, Aaron <osorio_arancibia.aaron_felipe@courrier.uqam.ca>
2024-10-17 15:06:35 -0400

    Ajouter solution labo6

--- LABO6/app.py
+++ LABO6/app.py
@@ -27,3 +27,62 @@ def deconnection():
     if database is not None:
         database.deconnection()
 
+def courriel_existe(courriel):
+    return get_db().courriel_existe(courriel)
+
+def valider_courriel(courriel, validation_courriel):
+    return courriel == validation_courriel
+
+def valider_mdp(str):
+    try:
+        if mdp_existant(str) is not None:
+            return True
+    except:
+        pass
+    return False
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    title = "Mon site - veuillez vous sinscrire"
+    if request.method == "GET":
+        return render_template("index.html", title=title), 200
+    else:
+
+        nom = request.form['nom']
+        prenom = request.form['prenom']
+        courriel = request.form['courriel']
+        validation_courriel = request.form['validation-courriel']
+        mdp = request.form['mdp']
+
+        if (nom == "" or prenom == "" or courriel == "" or validation_courriel == "" or mdp == ""):
+            message_erreur = "Erreur, tous les champs doivent être remplis"
+            return render_template("index.html", title=title,
+                                   message_erreur=message_erreur,
+                                   nom=nom, prenom=prenom, courriel=courriel), 400
+
+        if courriel_existe(courriel):
+            courriel_erreur = "Ce courriel existe deja"
+            return render_template("index.html",
+                                   title=title, courriel_erreur=courriel_erreur,
+                                   nom=nom, prenom=prenom, courriel=courriel), 400
+
+
+        salt = uuid.uuid4().hex
+        hashed_password = hashlib.sha512(str(mdp + salt).encode("utf-8")).hexdigest()
+        date_inscription = date.today()
+        get_db().inserer_utilisateur(nom, prenom, courriel, date_inscription, salt, hashed_password)
+        return redirect(url_for("confirmation", prenom=prenom)), 302
+
+
+
+@app.route("/confirmation/inscription")
+def confirmation():
+    title = "Mon site - vous êtes inscrit"
+    prenom = request.args.get("prenom")
+    return render_template("confirmation.html",
+                           prenom=prenom,
+                           title=title)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
--- LABO6/database.py
+++ LABO6/database.py
@@ -14,3 +14,24 @@ class Database():
     def deconnection(self):
         if self.connection is not None:
             self.connection.close()
+
+
+    def inserer_utilisateur(self, nom, prenom, courriel, date_inscription, salt, hashed_password):
+        connection = self.get_connection()
+        connection.execute(
+            "INSERT INTO utilisateur(nom, prenom, courriel, date_inscription, mot_de_passe_salt,mot_de_passe_hash) "
+                "VALUES (?, ?, ?, ?, ?, ?)",
+        (nom, prenom, courriel, date_inscription, salt, hashed_password)
+        )
+        connection.commit()
+
+    def courriel_existe(self, courriel):
+        cursor = self.get_connection().cursor()
+        cursor.execute("SELECT * FROM utilisateur WHERE courriel LIKE?", ('%' + courriel + '%',))
+        utilisateur_existe = cursor.fetchall()
+        if len(utilisateur_existe) == 0:
+            return False
+        else:
+            return True
+
+
